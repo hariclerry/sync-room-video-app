132b2c9d632ef46c8b03b5ce032c54e393dc4f99
hariclerry <clerryha@gmail.com>
2025-01-11 15:59:12 -0600

    change build folder

--- server/server.js
+++ server/server.js
@@ -25,10 +25,10 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
