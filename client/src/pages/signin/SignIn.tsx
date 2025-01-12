cadf2c8a5ada2dc1052fe1099412a8b173981140
hariclerry <clerryha@gmail.com>
2025-01-11 21:46:05 -0600

    add signin and signup url to env

--- client/src/pages/signin/SignIn.tsx
+++ client/src/pages/signin/SignIn.tsx
@@ -1,25 +1,12 @@
 import { SignIn } from '@clerk/clerk-react';
 import { Flex } from '@chakra-ui/react';
 
-import { useAuth } from '@clerk/clerk-react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
-
 const CustomSignIn = () => {
-  const { isSignedIn } = useAuth();
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  // useEffect(() => {
-  //   if (isSignedIn) {
-  //     const redirectTo = location.state?.from?.pathname || '/';
-  //     navigate(redirectTo, { replace: true });
-  //   }
-  // }, [isSignedIn, navigate, location.state]);
+  const signUpUrl = import.meta.env.VITE_CLERK_SIGN_UP_URL;
 
   return (
     <Flex marginTop="20" justifyContent={'center'}>
-      <SignIn signUpUrl="/sign-up" />
+      <SignIn signUpUrl={signUpUrl} />
     </Flex>
   );
 };
--- client/src/pages/signup/SignUp.tsx
+++ client/src/pages/signup/SignUp.tsx
@@ -2,9 +2,10 @@ import { SignUp } from '@clerk/clerk-react';
 import { Flex } from '@chakra-ui/react';
 
 function CustomSignUp() {
+  const signInUrl = import.meta.env.VITE_CLERK_SIGN_IN_URL;
   return (
     <Flex marginTop="20" justifyContent={'center'}>
-      <SignUp signInUrl="/sign-in" />
+      <SignUp signInUrl={signInUrl} />
     </Flex>
   );
 }
