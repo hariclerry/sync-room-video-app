65624a90e82c1a3432a9f6a20210c5ff3e551daf
hariclerry <clerryha@gmail.com>
2025-01-15 11:47:33 -0600

    add check for login

--- client/src/pages/signin/SignIn.tsx
+++ client/src/pages/signin/SignIn.tsx
@@ -1,7 +1,21 @@
-import { SignIn } from '@clerk/react-router';
+import { SignIn, useAuth } from '@clerk/react-router';
 import { Flex } from '@chakra-ui/react';
 
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 const CustomSignIn = () => {
+  const location = useLocation();
+  const { isSignedIn } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      const redirectTo = location.state?.from?.pathname || '/';
+      navigate(redirectTo, { replace: true });
+    }
+  }, [isSignedIn, navigate, location.state]);
+
   return (
     <Flex marginTop="20" justifyContent={'center'} position={'relative'}>
       <SignIn routing="path" path="/sign-in" signUpUrl="/sign-up"></SignIn>
